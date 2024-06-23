f2b18fdc18b771f0cca87998bba70e704ea72402
Sandipan Mahata <Sandipan.m@muvin.in>
2024-06-23 11:45:36 +0530

    added code for creating a blog

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -1,6 +1,6 @@
 """This file contains blog related views."""
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for
+    Blueprint, flash, g, redirect, render_template, request, url_for, session
 )
 from werkzeug.exceptions import abort
 
@@ -8,6 +8,15 @@ from flaskr.auth import login_required
 from flaskr.db import get_db
 
 bp = Blueprint("blog", __name__)
+
+@bp.before_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+    
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute("SELECT * from user WHERE id = ?", (user_id,)).fetchone()
 
 @bp.route("/")
 def index():
@@ -18,3 +27,27 @@ def index():
         ' ORDER BY created DESC'
     ).fetchall()
     return render_template("blog/index.html", posts=posts)
+
+@bp.route("/create", methods=["GET", "POST"])
+@login_required
+def create():
+    if request.method == "POST":
+        title = request.form["title"]
+        body = request.form["body"]
+        
+        error = None
+        if not title:
+            error = "Title is required"
+        
+        if not error:
+            db = get_db()
+            db.execute(
+                'INSERT INTO post (title, body, author_id)'
+                ' VALUES (?, ?, ?)',
+                (title, body, g.user['id'])
+            )
+            db.commit()
+            return redirect(url_for("blog.index"))
+        flash(error)
+    
+    return render_template("blog/create.html")
